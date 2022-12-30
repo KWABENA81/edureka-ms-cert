cf158d981adeabbb7b0065629276d588394c9bc2
Solomon Sefa (sask) <kwabena81@yahoo.com>
2022-12-30 03:44:14 -0500

    revise swagger implementation

--- bookms/src/main/java/com/edu/bookms/BookmsApplication.java
+++ bookms/src/main/java/com/edu/bookms/BookmsApplication.java
@@ -8,25 +8,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 //@EnableFeignClients
+@EnableSwagger2
+@EnableWebMvc
 @EnableEurekaClient
 @EnableWebSecurity
 public class BookmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookmsApplication.class, args);
+    }
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	@Bean
-	public WebClient webClient(){
-		return WebClient.builder().build();
-	}
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+
 }
