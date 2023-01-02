d6712f22b71ebce0245223a9e4cf7cccbbda04a9
Solomon Sefa (sask) <kwabena81@yahoo.com>
2023-01-02 04:05:03 -0500

    code for refinements

--- api-gateway/src/main/java/com/edu/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/edu/apigateway/ApiGatewayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
